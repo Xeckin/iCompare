65a688a20857879468c32f147b60b85c15d1fab6
Xeckin <73088071+Xeckin@users.noreply.github.com>
2023-02-04 17:15:44 -0500

    Update app.py
    Added compare route fix.

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
+import difflib
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/compare", methods=["GET", "POST"])
 def compare():
     if request.method == "POST":
-        file1 = None
-        file2 = None
-        encoding_types = ['utf-8', 'utf-16', 'utf-32']
-        for encoding in encoding_types:
+        file1 = request.files["file1"].read()
+        file2 = request.files["file2"].read()
+        try:
+            file1_lines = file1.decode("utf-8").splitlines()
+        except:
             try:
-                file1 = request.files["file1"].read().decode(encoding).splitlines()
-                file2 = request.files["file2"].read().decode(encoding).splitlines()
-                break
-            except UnicodeDecodeError:
-                continue
-        if file1 is None or file2 is None:
-            return "Error: The uploaded file is not in a supported encoding format. Please upload a file encoded in utf-8, utf-16, or utf-32."
-        
-        # Compare the contents of the two files
-        # ...
+                file1_lines = file1.decode("utf-16").splitlines()
+            except:
+                try:
+                    file1_lines = file1.decode("utf-32").splitlines()
+                except:
+                    return "Error: Unable to decode file1 as UTF-8, UTF-16, or UTF-32."
+        try:
+            file2_lines = file2.decode("utf-8").splitlines()
+        except:
+            try:
+                file2_lines = file2.decode("utf-16").splitlines()
+            except:
+                try:
+                    file2_lines = file2.decode("utf-32").splitlines()
+                except:
+                    return "Error: Unable to decode file2 as UTF-8, UTF-16, or UTF-32."
 
-        return "The comparison results."
-    return render_template("index.html")
+        diff = difflib.unified_diff(file1_lines, file2_lines, fromfile='file1', tofile='file2')
+
+        return render_template("compare.html", diff=diff)
 
 if __name__ == "__main__":
     app.run(debug=True)
