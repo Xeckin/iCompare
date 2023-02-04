389dbf05a734de85fe22e3d6ca239bd80df980af
Xeckin <73088071+Xeckin@users.noreply.github.com>
2023-02-04 17:10:30 -0500

    Update app.py
    Updated to accept multiple UTF formats.

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-# app.py
-
 from flask import Flask, request, render_template
-import difflib
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/compare", methods=["GET", "POST"])
+def compare():
+    if request.method == "POST":
+        file1 = None
+        file2 = None
+        encoding_types = ['utf-8', 'utf-16', 'utf-32']
+        for encoding in encoding_types:
+            try:
+                file1 = request.files["file1"].read().decode(encoding).splitlines()
+                file2 = request.files["file2"].read().decode(encoding).splitlines()
+                break
+            except UnicodeDecodeError:
+                continue
+        if file1 is None or file2 is None:
+            return "Error: The uploaded file is not in a supported encoding format. Please upload a file encoded in utf-8, utf-16, or utf-32."
+        
+        # Compare the contents of the two files
+        # ...
+
+        return "The comparison results."
     return render_template("index.html")
 
-@app.route("/compare", methods=["POST"])
-def compare():
-    file1 = request.files["file1"].read().decode("utf-8").splitlines()
-    file2 = request.files["file2"].read().decode("utf-8").splitlines()
-
-    diff = list(difflib.unified_diff(file1, file2))
-
-    return render_template("compare.html", diff=diff)
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
