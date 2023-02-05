6ca25fc2a2cb3fb72483f7dcffd25677376096b3
Xeckin <73088071+Xeckin@users.noreply.github.com>
2023-02-04 21:04:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,26 +12,27 @@ def compare():
     if request.method == "POST":
         file1 = request.files["file1"].read()
         file2 = request.files["file2"].read()
-        try:
-            file1_lines = file1.decode("utf-8").splitlines()
-        except:
+        file1_decoded = False
+        file2_decoded = False
+        decodings = ["utf-8", "utf-16", "utf-32"]
+        for encoding in decodings:
             try:
-                file1_lines = file1.decode("utf-16").splitlines()
+                file1_lines = file1.decode(encoding).splitlines()
+                file1_decoded = True
+                break
             except:
-                try:
-                    file1_lines = file1.decode("utf-32").splitlines()
-                except:
-                    return "Error: Unable to decode file1 as UTF-8, UTF-16, or UTF-32."
-        try:
-            file2_lines = file2.decode("utf-8").splitlines()
-        except:
+                continue
+        if not file1_decoded:
+            return "Error: Unable to decode file1 as UTF-8, UTF-16, or UTF-32."
+        for encoding in decodings:
             try:
-                file2_lines = file2.decode("utf-16").splitlines()
+                file2_lines = file2.decode(encoding).splitlines()
+                file2_decoded = True
+                break
             except:
-                try:
-                    file2_lines = file2.decode("utf-32").splitlines()
-                except:
-                    return "Error: Unable to decode file2 as UTF-8, UTF-16, or UTF-32."
+                continue
+        if not file2_decoded:
+            return "Error: Unable to decode file2 as UTF-8, UTF-16, or UTF-32."
 
         diff = difflib.unified_diff(file1_lines, file2_lines, fromfile='file1', tofile='file2')
 
